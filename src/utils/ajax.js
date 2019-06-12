37005130ca6fa7d257816ddef409600215b8889a
kdg <18811758306@163.com>
2019-06-13 00:04:35 +0800

    request url

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -2,7 +2,7 @@ import 'whatwg-fetch'
 import {$} from '../vendor';
 
 // 192.168.0.98
-export const AJAX_PATH = 'http://localhost:8080/ajax';
+export const AJAX_PATH = 'http://schooloms.com:8082/ajax';
 
 export default function (url, data) {
   return new Promise((resolve, reject) => {
